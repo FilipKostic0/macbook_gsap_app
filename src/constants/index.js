544cbef6b39989c58b508b1730e7e55e4b539434
Filip Kostic <fkostic00.w@gmail.com>
2025-11-19 17:59:46 +0100

    created navbar

---
+++ src/constants/index.js
@@ -0,0 +1,9 @@
+export const navLinks =
+    [
+        { label: 'Store' },
+        { label: 'Mac' },
+        { label: 'IPhone' },
+        { label: 'Watch' },
+        { label: 'Vision' },
+        { label: 'AirPods' }
+    ]
